f4c7471c5cb2d7a5e5143adfbb37c7d01fb64f65
lightning283 <deadtarget283@gmail.com>
2021-09-14 17:57:04 +0530

    install and uninstall scripts

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-cp apps/* ~/.local/share/applications/
+#!/usr/bin/env bash
+mkdir ~/.config/Max
+cp -r . ~/.config/Max
+sudo cp -r assets/ /bin/
+sudo cp max /bin/
+sudo chmod +x /bin/max
+sudo cp max_gui /bin/
+sudo chmod +x /bin/max_gui
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+rm -rf ~/.config/Max
+sudo rm -rf /bin/max
+sudo rm -rf /bin/max_gui
